e6d4e28d32176b784c69309d2bde0bda41707b60
casb62 <casb62@gmail.com>
2025-11-06 00:54:46 -0300

    Projeto criado

---
+++ src/main/java/com/casb/eBov/EBovApplication.java
@@ -0,0 +1,13 @@
+package com.casb.eBov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/casb/eBov/EBovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casb.eBov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
