30dab86c222a1839dc40fcde8094998f38b1f1fc
casb62 <casb62@gmail.com>
2025-11-06 02:26:21 -0300

    Banco de dados Postgresql e criação automatica da base de dados

--- src/main/java/com/casb/eBov/controller/resources/UserResource.java
+++ src/main/java/com/casb/eBov/controller/resources/UserResource.java
@@ -1,15 +1,26 @@
 package com.casb.eBov.controller.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.casb.eBov.model.domain.User;
 
 @RestController
 @RequestMapping(value="/usuarios")
 public class UserResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando. Que bacana!";
+	public List<User> listar() {
+		
+		User user1 = new User(1, "joao", "joao123");
+		User user2 = new User(2, "maria", "maria123");
+		
+		List<User> lista = new ArrayList<>();
+		lista.add(user1);
+		lista.add(user2);
+		
+		return lista;
 	}
 }
---
+++ src/main/java/com/casb/eBov/model/domain/User.java
@@ -0,0 +1,75 @@
+package com.casb.eBov.model.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String login;
+	private String password;
+	
+	public User() {
+		
+	}
+
+	public User(Integer id, String login, String password) {
+		super();
+		this.id = id;
+		this.login = login;
+		this.password = password;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {//Método possível de remoção. Acho que não pode ter ele aqui.
+
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
